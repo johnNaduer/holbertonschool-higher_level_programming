53d35e5eefe0adbf20d20b39592cffcc2e8e05d8
johnNaduer <naduer789@gmail.com>
2023-01-23 23:27:36 -0500

    style: improve code formatting

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__== "__main__":
     import sys
     n = len(sys.argv)
     i = 1
     cant = 0
-    while i < n :
+    while i < n:
         cant = cant + int(sys.argv[i])
         i = i + 1
     print("{:d}".format(cant))
