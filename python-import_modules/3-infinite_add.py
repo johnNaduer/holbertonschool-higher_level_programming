968fd1e100cf351d7aee1ccbe4b99ab3df7ad20d
johnNaduer <naduer789@gmail.com>
2023-01-23 23:21:56 -0500

    feat: add file 3-infinite_add.py also add function call argv to add numbers

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__=="__main__"
+    import sys
+    n = len(sys.argv)
+    i = 1
+    cant = 0
+    while i < n :
+        cant = cant + int(sys.argv[i])
+        i = i + 1
+    print("{:d}".format(cant))
