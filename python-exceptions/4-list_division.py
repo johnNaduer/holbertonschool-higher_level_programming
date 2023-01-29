acc589aa33270d325f0ddcb0dbc607d5c66b644f
johnNaduer <naduer789@gmail.com>
2023-01-28 22:59:42 -0500

    fix: correction function that divides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    x = 0
-    while x < list_length:
+    co = 0
+    length = []
+    while co < list_length:
         try:
-            if my_list_2[x] == 0:
-                print("division by 0")
-            if type(my_list_1[x]) != type(my_list_2[x]):
-                print("wrong type")
-            y = my_list_1[x] / my_list_2[x]
-            new_list.append(y)
+            res = my_list_1[co] / my_list_2[co]
+        except ZeroDivisionError:
+            res = 0
+            print("division by 0")
+        except TypeError:
+            res = 0
+            print("wrong type")
         except IndexError:
+            res = 0
             print("out of range")
-            new_list.append(0)
-        except:
-            new_list.append(0)
-        x = x + 1
-    return (new_list)
+        finally:
+            co += 1
+            length.append(res)
+    return length
