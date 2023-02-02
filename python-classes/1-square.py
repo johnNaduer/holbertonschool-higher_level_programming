b4b6cd30f5129230ce6c3e5f947a33d4b985e9bd
johnNaduer <naduer789@gmail.com>
2023-02-02 17:06:10 -0500

    feat: add comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Defines the square class"""
+
+
+
 class Square:
+    """Square class. Has a size"""
     def __init__(self, size):
-        self.__size = size
+    self.__size = size
