b03237e5bf162213cfe7b05655a62468a335fc51
johnNaduer <naduer789@gmail.com>
2023-02-02 12:33:29 -0500

    fix:whitespace standard PEP8

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 x = ord('z')
 y = ord('Y')
 while x >= ord('a') and y >= ord('A'):
-    print("{}{}".format(chr(x),chr(y)),end="")
+    print("{}{}".format(chr(x), chr(y)), end="")
     x = x - 2
     y = y - 2
