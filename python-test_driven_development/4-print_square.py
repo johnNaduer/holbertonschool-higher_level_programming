b413b7d6fcaa154bfc420c5555da28f21a0a5eb1
johnNaduer <naduer789@gmail.com>
2023-02-03 18:30:15 -0500

    feat: add file 4-print_square.py that containing function that prints a square with the character #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this module containing a function that prints a square"""
+
+
+def print_square(size):
+    
+    """Prints a square made of "#" characters with the given size"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0 :
+        raise ValueError("size must be an integer")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    x = 0
+    while x < size:
+        y = 0
+        while y < size:
+           print("#",end="")
+           y = y + 1
+        print("")
+        x = x + 1
