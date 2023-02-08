c67847e575b35f43deea512d724f3de8cdca3beb
johnNaduer <naduer789@gmail.com>
2023-02-08 10:31:14 -0500

    fix: correnting code add operators in the print

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
         num1 = int(sys.argv[1])
         num2 = int(sys.argv[3])
         if sys.argv[2] == "+":
-            print("{}".format(calculator_1.add(num1,num2)))
+            print("{} + {} = {}".format(num1, num2, calculator_1.add(num1,num2)))
         elif sys.argv[2] == "-":
-            print("{}".format(calculator_1.sub(num1,num2)))
+            print("{} - {} = {}".format(num1, num2, calculator_1.sub(num1,num2)))
         elif sys.argv[2] == "*":
-            print("{}".format(calculator_1.mul(num1,num2)))
+            print("{} * {} = {}".format(num1, num2, calculator_1.mul(num1,num2)))
         elif sys.argv[2] == "/":
-            print("{}".format(calculator_1.div(num1,num2)))
+            print("{} / {} = {}".format(num1, num2, calculator_1.div(num1,num2)))
     else:
         print("{}".format(1))
