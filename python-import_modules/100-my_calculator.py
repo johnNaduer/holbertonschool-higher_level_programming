ed6d55ee2960c8a626f71d59a7f71bc3be8676bf
johnNaduer <naduer789@gmail.com>
2023-02-08 11:07:17 -0500

    fix: correcting standard PEP8

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,16 +7,16 @@ if __name__ == "__main__":
         num2 = int(sys.argv[3])
 
         if sys.argv[2] == "+":
-            print("{} + {} = {}".format(num1, num2, calculator_1.add(num1,num2)))
+            print("{} + {} = {}".format(num1, num2, calculator_1.add(num1, num2)))
         elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(num1, num2, calculator_1.sub(num1,num2)))
+            print("{} - {} = {}".format(num1, num2, calculator_1.sub(num1, num2)))
         elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(num1, num2, calculator_1.mul(num1,num2)))
+            print("{} * {} = {}".format(num1, num2, calculator_1.mul(num1, num2)))
         elif sys.argv[2] == "/":
             if num2 == 0:
                 exit(1)
             else:
-                print("{} / {} = {}".format(num1, num2, calculator_1.div(num1,num2)))
+                print("{} / {} = {}".format(num1, num2, calculator_1.div(num1, num2)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
