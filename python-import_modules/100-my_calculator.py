3e07dc297cd59d67b44b2f88cc40150b11da45a1
johnNaduer <naduer789@gmail.com>
2023-02-08 10:58:35 -0500

    fix: correcting code add exit()

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 4:
         num1 = int(sys.argv[1])
         num2 = int(sys.argv[3])
+
         if sys.argv[2] == "+":
             print("{} + {} = {}".format(num1, num2, calculator_1.add(num1,num2)))
         elif sys.argv[2] == "-":
@@ -13,8 +14,11 @@ if __name__ == "__main__":
             print("{} * {} = {}".format(num1, num2, calculator_1.mul(num1,num2)))
         elif sys.argv[2] == "/":
             if num2 == 0:
-                sys.exit(1)
+                exit(1)
             else:
                 print("{} / {} = {}".format(num1, num2, calculator_1.div(num1,num2)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
     else:
-        sys.exit(1)
+        exit(1)
