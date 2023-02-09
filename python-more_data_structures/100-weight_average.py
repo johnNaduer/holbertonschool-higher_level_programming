5dacd7df884dc91c57001a5fcaf016dcbf584a14
johnNaduer <naduer789@gmail.com>
2023-02-09 09:01:55 -0500

    feat: add file 100-weight_average.py that contains function that returns the weighted average of all integers tuple

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x = 0
+    suma = 0
+    suma2 = 0
+    while x < len(my_list):
+        numero = 1
+        y = 0
+        while y < len(my_list[x]):
+            numero1 = my_list[x][y]
+            numero = my_list[x][y] * numero
+            y = y + 1
+            if y < len(my_list[x]):
+                suma2 = my_list[x][y] + suma2
+        suma = numero + suma
+        x = x + 1
+    return suma/suma2
