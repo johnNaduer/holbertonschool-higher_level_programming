12cad465da2cebcd2aeb687ab4d176fa68767725
johnNaduer <naduer789@gmail.com>
2023-02-09 09:05:44 -0500

    fix: correting condition

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
     x = 0
     suma = 0
     suma2 = 0
@@ -14,4 +16,5 @@ def weight_average(my_list=[]):
                 suma2 = my_list[x][y] + suma2
         suma = numero + suma
         x = x + 1
-    return suma/suma2
+    if suma2 != 0:
+        return suma/suma2
