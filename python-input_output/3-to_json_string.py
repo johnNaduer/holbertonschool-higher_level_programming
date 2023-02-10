370a5b2b93c20621a7c527d3efc757d13d6c9604
johnNaduer <naduer789@gmail.com>
2023-02-10 18:12:49 -0500

    feat: add fiel that contains function that returns the JSON representation of an object (string)

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Defines a function that converts an object to a json string"""
 import json
 
+
 def to_json_string(my_obj):
     """Converts an object to a json string"""
     new_json = json.dumps(my_obj)
