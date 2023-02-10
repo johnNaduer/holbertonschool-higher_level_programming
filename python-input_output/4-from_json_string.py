f032aea48d8e274a5fcf351064b113b0ada4fec6
johnNaduer <naduer789@gmail.com>
2023-02-10 18:21:20 -0500

    fix: standard PEP8

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,7 +2,8 @@
 """Defines a function that converts an object to a json string"""
 import json
 
+
 def from_json_string(my_str):
-    """Converts an object to a json string"""    
+    """Converts an object to a json string"""
     new_objetc = json.loads(my_str)
     return (new_objetc)
