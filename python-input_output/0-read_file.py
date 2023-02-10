2422434baa7e45057e12acc2737f553e1fbd9ac6
johnNaduer <naduer789@gmail.com>
2023-02-10 00:03:22 -0500

    fix: correcting standar PEP8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -14,12 +14,12 @@ def read_file(filename=""):
     Returns:
     None
     """
-    with open(filename, 'r', encoding ='utf-8') as file:
+    with open(filename, 'r', encoding = 'utf-8') as file:
         """
-        Open the file specified in the 'filename' argument with 'r' mode 
-        (read mode)
-        and specify the encoding as 'utf-8'. The with statement is used to ensure
+        Open the file specified in the 'filename' argument with 'r' mode
+        (read mode)and specify the encoding as 'utf-8'. The with
+        statement is used to ensure
         that the file is properly closed after the contents have been read.
         """
         file_contents = file.read()
-        print(file_contents,end="")
+        print(file_contents, end="")
