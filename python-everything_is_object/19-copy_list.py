4868058194d900add01f16cb88cf037e8a0ed7c2
johnNaduer <naduer789@gmail.com>
2023-02-12 10:37:14 -0500

    fix:update code

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-""" function def copy_list(a_list): that returns a copy of a list """
-
-
 def copy_list(a_list):
-    """ function that return copy a_list """
   return a_list[:]
