0ba63e29360d8d40959b2ae2410293a6e7a0a40a
johnNaduer <naduer789@gmail.com>
2023-02-16 18:36:50 -0500

    feat: add file that contains class Rectangle by adding the public method def update(self, *args)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -153,3 +153,19 @@ class Rectangle(Base):
         """method __str__ print rectangle atributes"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
         self.width, self.height)
+
+    def update(self, *args):
+        """ method print args """
+        if args:
+            self.id = args[0]
+        if len(args) >= 2 and args[1] is not None:
+            self.width = args[1]
+        if len(args) >= 3 and args[2] is not None:
+            self.height = args[2]
+        if len(args) >= 4 and args[3] is not None:
+            self.x = args[3]
+        if len(args) >= 5 and args[4] is not None:
+            self.y = args[4]
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+        self.width, self.height)
