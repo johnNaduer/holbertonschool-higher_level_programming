476553a1a716bd8d64cb3284fe98885df66a3e14
johnNaduer <naduer789@gmail.com>
2023-02-16 22:12:41 -0500

    feat: add file that contains  class Square that inherits from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square, inheriting from Rectangle."""
+
+     def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a Square object."""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """Returns a string representation of the square.
+
+        Returns:
+            str: A string in the format '[Square] (<id>) <x>/<y> - <size>'.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
