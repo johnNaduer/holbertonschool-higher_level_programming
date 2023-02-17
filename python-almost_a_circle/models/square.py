b25f60e1ce65232b87634f6246a42763b20414e9
johnNaduer <naduer789@gmail.com>
2023-02-16 22:19:52 -0500

    fix: PEP8 standard

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,4 +17,5 @@ class Square(Rectangle):
         Returns:
             str: A string in the format '[Square] (<id>) <x>/<y> - <size>'.
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, \
+        self.size)
