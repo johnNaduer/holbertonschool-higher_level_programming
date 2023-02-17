87f6dba31c43fcba4d6587d852000984f5844988
johnNaduer <naduer789@gmail.com>
2023-02-17 12:38:16 -0500

    feat: add file that contains class Square by adding the public getter and setter size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,7 +9,24 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a Square object."""
         super().__init__(size, size, x, y, id)
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
         self.size = size
+
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")      
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Returns a string representation of the square.
@@ -17,5 +34,4 @@ class Square(Rectangle):
         Returns:
             str: A string in the format '[Square] (<id>) <x>/<y> - <size>'.
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,\
-        self.size)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
