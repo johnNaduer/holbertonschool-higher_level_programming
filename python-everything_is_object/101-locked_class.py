a34ed4aa2a5966e005083ea654ee4d40444adee8
johnNaduer <naduer789@gmail.com>
2023-02-19 12:43:07 -0500

    fix: update code

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -6,5 +6,5 @@ class LockedClass:
     """class that create attribute first_name """
     def __setattr__(self, attr, val):
         if attr != 'first_name':
-            raise AttributeError(f"{self.__class__.__name__} has no attribute '{attr}'")
+            raise AttributeError("'LockedClass' object has no attribute '" + attr + "'")
         self.__dict__[attr] = val
