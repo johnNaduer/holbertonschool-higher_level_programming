69cce6ca23eb228923288beb0a98daf86e634283
johnNaduer <naduer789@gmail.com>
2023-03-13 06:36:06 -0700

    feat:  class Student that defines a student by

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""  class Student that defines a student """
 
 class Student:
-
+    """ class that contains __init__ and to_json"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
