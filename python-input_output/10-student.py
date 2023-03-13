5d0048a59374c08175dddd4215eb78e813863f63
johnNaduer <naduer789@gmail.com>
2023-03-13 06:34:32 -0700

    feat:  class Student that defines a student by

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        mydict = {}
+        data1 = 'first_name'
+        data2 = 'last_name'
+        data3 = 'age'
+        if attrs:
+            if data1 in attrs:
+                mydict[data1] = self.first_name
+            if data2 in attrs:
+                mydict[data2] = self.last_name
+            if data3 in attrs:
+                mydict[data3] = self.age
+            return mydict
+        else:
+            return {'first_name':self.first_name, 'last_name': self.last_name, 'age': self.age}
+
