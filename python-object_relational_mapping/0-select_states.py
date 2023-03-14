5c78aa3b97d9c377786bf89c2be1816b670499d5
johnNaduer <naduer789@gmail.com>
2023-03-14 15:58:06 -0700

    feat: script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,10 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        port = 3306,
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3]
-        )
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
