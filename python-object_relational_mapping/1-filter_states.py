52a2383686ae74900fca965490ad3cf0f45840d2
johnNaduer <naduer789@gmail.com>
2023-03-14 19:45:56 -0700

    feat: script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
 if __name__=='__main__':
     db = MySQLdb.connect(
-        port = 3306,
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3]
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
         )
 
     cursor = db.cursor()
