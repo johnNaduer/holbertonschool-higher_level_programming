746eb62c281cb6f42de90b7f903d10c67e921725
johnNaduer <naduer789@gmail.com>
2023-03-15 08:24:36 -0700

    feat: add SQL Injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python3
-""" Script that takes in arguments and displays all values in the states
-    table of hbtn_0e_0_usa where name matches the argument. But this time,
-    write one that is safe from MySQL injections! """
+"""
+Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument. This script is
+safe from MySQL injections!
+"""
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name)
+    # Connect to MySQL server
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name,
+        charset="utf8"
+    )
 
-    cur = db.cursor()
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
 
-    query = """SELECT * FROM states
-               WHERE name LIKE BINARY %(state_name)s
-               ORDER BY id ASC"""
+    # Execute the query (safe from SQL injection)
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name,))
 
-    cur.execute(query, {'state_name': state_name})
+    # Fetch all the rows as a list of tuples
+    rows = cur.fetchall()
 
-    for row in cur.fetchall():
+    # Display the results
+    for row in rows:
         print(row)
 
+    # Close cursor and connection
     cur.close()
-    db.close()
+    conn.close()
