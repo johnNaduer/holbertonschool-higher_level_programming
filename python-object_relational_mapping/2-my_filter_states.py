64710422bd733c4e854cd4eb0ce83dcc92f7397d
johnNaduer <naduer789@gmail.com>
2023-03-14 21:15:33 -0700

    feat: script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    db.close()
+    db.cursor()
