f691c3071e9dccb5391f24c20d57951bacf66565
johnNaduer <naduer789@gmail.com>
2023-03-15 19:51:05 -0700

    feat: script that lists all cities from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        );
+
+    cursor = db.cursor()
+    i = "SELECT cities.id, cities.name, states.name FROM cities JOIN\
+        states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    cursor.execute(i)
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.cursor()
