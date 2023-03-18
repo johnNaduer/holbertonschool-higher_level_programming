0910f9a0aee487d5dcdd7a6157fb8ce4e2f4f955
johnNaduer <naduer789@gmail.com>
2023-03-18 07:44:38 -0700

    feat: script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,12 +8,9 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
-
     i = 'mysql+mysqldb://{}:{}@localhost/{}'
-
-    engine = create_engine(i.
-            format(sys.argv[1], sys.argv[2], sys.argv[3]), 
-            pool_pre_ping=True)
+    engine = create_engine(i.format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
