a1e940ce0eab54f39d6bddee377f31c605bbf341
johnNaduer <naduer789@gmail.com>
2023-03-19 13:13:09 -0700

    feat: script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Deletes into State obj from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+"""Fetch all states using sqlalchemy"""
+
+
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy import func
+from sys import argv
 
-
-def delete_a_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-
-    rows = session.query(State).all()
-
-    for i in rows:
-        if 'a' in i.__dict__['name']:
-            session.delete(i)
-
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    qry = session.query(State).filter(State.name.contains(func.binary("a")))
+    for record in qry.all():
+        session.delete(record)
     session.commit()
-
-    session.close()
-
-if __name__ == "__main__":
-    delete_a_state_obj()
