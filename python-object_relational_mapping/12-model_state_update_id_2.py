4750dbb5a0faeaac5c3439f34046aec418ee1cd7
johnNaduer <naduer789@gmail.com>
2023-03-19 13:08:48 -0700

    feat: script that changes the name of a State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
+"""Fetch all states using sqlalchemy"""
 
-import sys
+
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy import func
+from sys import argv
 
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print('Usage: {} username password database_name'.format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, db_name),
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).filter_by(id == 2).first()
-    if state:
-        state.name = 'New Mexico'
-        session.add(state)
-        session.commit()
-    session.close()
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.add(state)
+    session.commit()
