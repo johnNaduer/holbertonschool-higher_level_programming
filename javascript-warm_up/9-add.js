fda339f978f76af5a11920198d392327772415fc
johnNaduer <naduer789@gmail.com>
2023-04-17 08:43:00 -0700

    feat: update add 9-add.js

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function add(a, b) {
+  return a + b;
+}
+const [, , arg1, arg2] = process.argv;
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Invalid arguments");
+} else {
+  console.log(add(num1, num2));
+}
