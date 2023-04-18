07a11899d1e15a0c8838549f933bf53067100513
johnNaduer <naduer789@gmail.com>
2023-04-17 23:24:12 -0500

    feat: script that reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-
+const argv = process.argv;
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) {
+const fileName = argv[2];
+fs.readFile(fileName, 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
-    return;
+    return console.log(err);
   }
-  console.log(data);
+  process.stdout.write(data);
 });
