ad6b919938e3a906b634a00bd624fc2d67dc97b0
johnNaduer <naduer789@gmail.com>
2023-04-18 09:12:35 -0500

    feat: update code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const argv = process.argv;
 const fs = require('fs');
-const fileName = argv[2];
-fs.readFile(fileName, 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  process.stdout.write(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) { console.log(err); } else { console.log(data); }
 });
