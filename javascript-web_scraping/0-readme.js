2b1231e13636a8b51d357a74594000ec13f8d0cc
johnNaduer <naduer789@gmail.com>
2023-04-18 09:55:05 -0500

    feat: update code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) { console.log(err); } else { console.log(data); }
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
